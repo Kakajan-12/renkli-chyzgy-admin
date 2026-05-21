668f9dd0f196aa17254af852ed92812a6c96ca56
Mamagul <mamagul.gapurova@mail.ru>
2026-05-21 12:38:13 +0500

    add new tiptap

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,26 @@
 module.exports = {
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'api.bn-tour.com',
-    //             pathname: '/uploads/**',
-    //         },
-    //     ],
-    // },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '3001',
-                pathname: '/uploads/**',
-            },
-        ],
-    },
+  // images: {
+  //     remotePatterns: [
+  //         {
+  //             protocol: 'https',
+  //             hostname: 'api.bn-tour.com',
+  //             pathname: '/uploads/**',
+  //         },
+  //     ],
+  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3002",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.renklicyzgy.com",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
